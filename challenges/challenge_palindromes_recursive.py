7e2bc02cba43aa919ad59d7cafe07f21efa4ff4a
Katia Nienow <nienowkatia@gmail.com>
2023-09-24 19:29:51 -0300

    requisito 03 - ok

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    wordLower = word.lower()
+    size = len(wordLower)
+    if size == 0:
+        return False
+
+    if low_index == high_index:
+        inverted_word = wordLower[::-1]
+        if inverted_word == wordLower:
+            return True
+        else:
+            return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index)
