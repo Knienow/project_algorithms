3d7a96f553d975d0f802ca17ec7cadbb689d5be7
Katia Nienow <nienowkatia@gmail.com>
2023-09-24 18:06:34 -0300

    fix lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,8 @@
-#boas práticas para legibilidade:
-#Entre cada função temos um espaço de 2 linhas;
-#As funções são declaradas com nomes em letras minúsculas;
-#O import é utilizado para termos todas as funções do módulo disponíveis em outro arquivo;
-#https://www.w3schools.com/python/python_try_except.asp
-    #O try permite testar um bloco de código em busca de erros.
-    #O except permite lidar com o erro.
-
 def study_schedule(permanence_period, target_time):
     try:
         counter = 0
-        for start, end in permanence_period: # vamos iterar a lista completa
-            if start <= target_time <= end: # se encontrar o elemento alvo, retorne a posição
+        for start, end in permanence_period:
+            if start <= target_time <= end:
                 counter += 1
         return counter
     except TypeError:
