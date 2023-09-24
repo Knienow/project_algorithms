818ad6440659de41f9cf6e696396f33e9112d800
Katia Nienow <nienowkatia@gmail.com>
2023-09-24 20:45:16 -0300

    requisito 04 em andamento - pesquisando substituto para o sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,17 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    list_first_string = list(first_string)
+    list_second_string = list(second_string)
+
+    list_first_string.sort()
+    list_second_string.sort()
+
+    position = 0
+    equals = True
+
+    while position < len(first_string) and equals:
+        if list_first_string[position] == list_second_string[position]:
+            position = position + 1
+        else:
+            equals = False
+
+    return equals
