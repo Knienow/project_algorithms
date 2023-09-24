842a537a9d6626d86377d956932b1d06201f515f
Katia Nienow <nienowkatia@gmail.com>
2023-09-24 17:58:59 -0300

    requisito 01 - ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
+#boas práticas para legibilidade:
+#Entre cada função temos um espaço de 2 linhas;
+#As funções são declaradas com nomes em letras minúsculas;
+#O import é utilizado para termos todas as funções do módulo disponíveis em outro arquivo;
+#https://www.w3schools.com/python/python_try_except.asp
+    #O try permite testar um bloco de código em busca de erros.
+    #O except permite lidar com o erro.
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        counter = 0
+        for start, end in permanence_period: # vamos iterar a lista completa
+            if start <= target_time <= end: # se encontrar o elemento alvo, retorne a posição
+                counter += 1
+        return counter
+    except TypeError:
+        return None
