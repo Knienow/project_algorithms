42d7297771ddd936f35a48f37dffb1d3ba001cd6
Katia Nienow <nienowkatia@gmail.com>
2023-09-26 22:23:27 -0300

    requisito 04 - ok

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,17 +1,52 @@
-def is_anagram(first_string, second_string):
-    list_first_string = list(first_string)
-    list_second_string = list(second_string)
+def spread_caractere(string):
+    # string = string.lower()
+    if len(string) <= 1:
+        return string
+    middle = len(string) // 2
+    left = string[:middle]
+    right = string[middle:]
 
-    list_first_string.sort()
-    list_second_string.sort()
+    left = spread_caractere(left)
+    right = spread_caractere(right)
+    result = before_sort_after_merge(left, right)
+    return result
 
-    position = 0
-    equals = True
 
-    while position < len(first_string) and equals:
-        if list_first_string[position] == list_second_string[position]:
-            position = position + 1
+def before_sort_after_merge(left, right):
+    word = []
+    index_l = 0
+    index_r = 0
+    while index_l < len(left) and index_r < len(right):
+        if left[index_l] < right[index_r]:
+            word.append(left[index_l]) 
+            index_l += 1
         else:
-            equals = False
+            word.append(right[index_r])
+            index_r += 1
+    word.extend(left[index_l:])
+    word.extend(right[index_r:])
+    return word
 
-    return equals
+
+def join_sorted_string(string):
+    string = list(string)
+    empty = ''
+    list_string = empty.join(spread_caractere(string))
+    return list_string
+
+
+def is_anagram(first_string, second_string):
+    first_string = first_string.casefold()
+    second_string = second_string.casefold()
+    first_str_spread = join_sorted_string(first_string)
+    second_str_spread = join_sorted_string(second_string)
+    #se alguma das strings estiver vazia, retorna False
+    if not first_string or not second_string:
+        return (first_str_spread, second_str_spread, False)
+    #comparando o tamanho das duas strings para verificar se ambas possuem o mesmo comprimento
+    if len(first_string) != len(second_string):
+        return (first_str_spread, second_str_spread, False)
+    if first_str_spread == second_str_spread:
+        return (first_str_spread, second_str_spread, True)
+    else:
+        return (first_str_spread, second_str_spread, False)
